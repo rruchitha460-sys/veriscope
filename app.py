046c9cf5e97e50c6bcce1516a04a5fc5c26639e4
rruchitha460-sys <rruchitha460@gmail.com>
2026-05-04 13:01:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from langchain_community.document_loaders import PyMuPDFLoader
 from langchain_core.documents import Document
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import FAISS
-from langchain.embeddings.huggingface import HuggingFaceEmbeddings
+from langchain_community.embeddings import HuggingFaceEmbeddings
 from openai import OpenAI
 import tempfile
 import random
